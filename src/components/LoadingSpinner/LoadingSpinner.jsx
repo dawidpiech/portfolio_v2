986b759d7459f20b06c5d60a933def2a748a79f4
Dawid Piech <dawid.piech243@gmail.com>
2024-03-20 00:40:45 +0100

    Add Loading spinner

--- src/components/LoadingSpinner/LoadingSpinner.jsx
+++ src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 
-function LoadingSpinner() {
-
+function LoadingSpinner({ isLoading }) {
   const LoadingSpinner = styled.div`
+    ${(props) => console.log(props.isLoading)}
     top: 0;
     left: 0;
     position: fixed;
     width: 100%;
     height: 100vh;
-    background: ${props => props.theme.colors[0]};
+    background: ${(props) => props.theme.colors[4]};
     z-index: 99999;
+    animation: ${(props) =>
+      props.isLoading ? "" : "scrollToTop 1s ease-in-out forwards"};
 
     & > div {
       position: relative;
@@ -18,7 +20,7 @@ function LoadingSpinner() {
       top: calc(50% - 45px);
       width: 70px;
       height: 70px;
-      fill: ${props => props.theme.colors[2]};
+      fill: ${(props) => props.theme.colors[2]};
     }
 
     & > div > div:nth-child(2) {
@@ -30,8 +32,8 @@ function LoadingSpinner() {
       margin: -150px 0 0 -75px;
       border-radius: 50%;
       border: 3px solid transparent;
-      border-top-color: ${props => props.theme.colors[2]};
-      border-left-color: ${props => props.theme.colors[2]};
+      border-top-color: ${(props) => props.theme.colors[2]};
+      border-left-color: ${(props) => props.theme.colors[2]};
       animation: spin 2s linear infinite;
     }
 
@@ -43,37 +45,56 @@ function LoadingSpinner() {
         transform: rotate(360deg);
       }
     }
-  `
+
+    @keyframes scrollToTop {
+      0% {
+        top: 0;
+      }
+      100% {
+        top: -100%;
+      }
+    }
+  `;
 
   return (
-    <LoadingSpinner>
+    <LoadingSpinner isLoading={isLoading}>
       <div>
         <div>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 136 189">
-
             <mask id="mask">
               <rect width="136" height="189" fill="white" />
-              <path fill="black" d="M 68.00,82.00
+              <path
+                fill="black"
+                d="M 68.00,82.00
                             C 68.00,82.00 61.03,21.54 78.43,21.69
-                            100.72,21.20 68.94,81.09 68.00,82.00 Z" />
-              <path fill="black" d="M 88.20,103.68
+                            100.72,21.20 68.94,81.09 68.00,82.00 Z"
+              />
+              <path
+                fill="black"
+                d="M 88.20,103.68
                             C 89.92,89.86 104.44,84.15 103.86,84.08
                             110.01,80.84 124.76,76.65 123.00,93.00
                             118.29,110.98 100.07,123.51 97.19,125.81
                             94.00,127.26 94.91,127.07 90.64,128.53
                             89.75,126.80 89.78,126.43 89.37,125.16
-                            87.86,120.45 86.82,114.68 88.20,103.68 Z" />
-              <path fill="black" d="M 53.00,105.00
+                            87.86,120.45 86.82,114.68 88.20,103.68 Z"
+              />
+              <path
+                fill="black"
+                d="M 53.00,105.00
                             C 55.87,99.94 58.03,95.70 56.97,87.01
                             54.03,80.57 54.11,80.88 50.90,77.98
                             47.98,75.52 43.95,72.00 39.51,72.50
                             35.08,73.00 29.70,75.77 27.07,78.96
                             24.45,82.14 16.60,94.48 15.75,102.92
                             14.89,111.38 23.82,117.15 32.27,116.09
-                            40.15,115.09 50.12,110.06 53.00,105.00 Z" />
+                            40.15,115.09 50.12,110.06 53.00,105.00 Z"
+              />
             </mask>
 
-            <path mask="url(#mask)" d="M 60.46,75.11
+            <path
+              mask="url(#mask)"
+              d="M 60.46,75.11
                             C 60.46,75.11 50.53,66.75 44.69,65.24
                             38.86,63.73 35.21,62.85 28.46,66.68
                             20.36,70.34 8.11,77.11 1.96,98.91
@@ -91,7 +112,8 @@ function LoadingSpinner() {
                             68.00,101.00 90.11,53.16 98.33,35.43
                             106.56,17.71 89.34,-12.93 69.03,6.92
                             48.72,26.78 59.09,61.79 59.09,61.79
-                            59.09,61.79 60.46,75.11 60.46,75.11 Z" />
+                            59.09,61.79 60.46,75.11 60.46,75.11 Z"
+            />
           </svg>
         </div>
         <div></div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import AboutSection from "../components/AboutSection/AboutSection";
 import HomeSection from "../components/HomeSection/HomeSection";
@@ -7,14 +7,24 @@ import SkillsSection from "../components/SkillsSection/SkillsSection";
 import MyProjectSection from "../components/MyProjectSection/MyProjectSection";
 import ContactSection from "../components/ContactSection/ContactSection";
 import Footer from "../components/Footer/Footer";
+import LoadingSpinner from "../components/LoadingSpinner/LoadingSpinner";
 
 function Home(props) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
+
   const Layout = styled.div`
     overflow-x: hidden;
   `;
 
   return (
     <Layout>
+      <LoadingSpinner isLoading={isLoading}></LoadingSpinner>
       <Navigation></Navigation>
       <HomeSection changeTheme={props.changeTheme}></HomeSection>
       <AboutSection></AboutSection>
